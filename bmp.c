c7751f192624a02c99d2350552b1d8279c6dbcbe
tamas-fekete <89tamas.fekete@gmail.com>
2019-07-13 11:00:40 +0200

    wtf something is not working

--- bmp.c
+++ bmp.c
@@ -5,8 +5,8 @@
 #include <unistd.h>
 #pragma pack ( 1 )
 
-#define height 4
-#define width 4
+#define height 600
+#define width 800
 #define bytesperpixel 3
 
 typedef struct{
@@ -43,14 +43,46 @@ typedef struct{
   PixelData_t pixeldata;
 }BMP_t;
 
+typedef struct{
+  char red;
+  char green;
+  char blue;
+}color_t;
+
+typedef struct{
+ int sx;
+ int sy;
+ int sz;
+ int r;
+ color_t color; 
+}sphere_t;
+typedef struct{
+  int x1;
+  int y1;
+  int z1;
+  int x2;
+  int y2;
+  int z2;
+}line_t;
+
 void putpixel(BMP_t* image,int x, int y, char red, char green, char blue);
+char LineIntersectsSphere(line_t *line, sphere_t *sphere);
 int main(void)
 {
-
   BMP_t image;
   int fd;
   int bytes;
-  unsigned int i;
+  unsigned int i, j;
+  sphere_t sphere;
+  line_t line;
+  sphere.sx  = 100;
+  sphere.sy  = 100;
+  sphere.sz  = 100;
+  sphere.r = 1000;
+  line.x1 = width/2;
+  line.y1 = height/2;
+  line.z1 = 0;
+  line.z2 = 1;
   image.header.Signature = 0x4D42;
   image.header.FileSize  = sizeof(image);
   image.header.reserved  = 0;
@@ -79,14 +111,23 @@ int main(void)
     image.pixeldata.datab[i]=0;
   }
  
-  putpixel(&image, 0, 0, 255, 255, 255);
-  putpixel(&image, 0, 1, 0, 0, 0);
-  putpixel(&image, 0, 2, 255, 255, 255);
-  putpixel(&image, 0, 3, 0, 0, 0);
+  for(i=0; i<600; i++)
+  {
+    for(j=0; j<800; j++)
+    {
+      line.x2 = j;
+      line.y2 = i;
+      if(LineIntersectsSphere(&line, &sphere))
+      {
+        putpixel(&image, j, 599-i, 0xA4, 0x69, 0xFF);
+      }
+      else
+      {
+        putpixel(&image, j, 599-i, 0xFF, 0xFF, 0xFF);
+      }
+    }
+  }
 
-  putpixel(&image, 1, 0, 0, 0, 0);
-  putpixel(&image, 2, 0, 255, 255, 255);
-  putpixel(&image, 3, 0, 0, 0, 0);
   if((bytes=write(fd, &image.header, sizeof(image))) < sizeof(image))
   {
      printf("bytes written: %d. Size: %ld\n", bytes, sizeof(image));
@@ -111,4 +152,15 @@ void putpixel(BMP_t* image,int x, int y, char red, char green, char blue)
   image->pixeldata.data[y][x][0] = blue;
   image->pixeldata.data[y][x][1] = green;
   image->pixeldata.data[y][x][2] = red;
+}
+char LineIntersectsSphere(line_t *line, sphere_t *sphere)
+{
+  int a = (line->x2-line->x1)*(line->x2-line->x1)+(line->y2-line->y1)*(line->y2-line->y1)+(line->z2-line->z1)*(line->z2-line->z1);
+  int b = 2*((line->x2-line->x1)*(line->x1-sphere->sx) + (line->y2-line->y1)*(line->y1-sphere->sy) + (line->z2-line->z1)*(line->z1-sphere->sz));
+  int c = sphere->sx*sphere->sx+sphere->sy*sphere->sy+sphere->sz*sphere->sz+line->x1*line->x1+line->y1*line->y1+line->z1*line->z1-2*(sphere->sx*line->x1+sphere->sy*line->y1+sphere->sz*line->z1) - sphere->r*sphere->r;
+  if( (b*b-4*a*c) < 0)
+  {
+    return 0;
+  }
+  else return 1;
 }
