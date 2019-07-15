3dcf816ed7899224dcbff3e8b9fbd421a58c62ba
tamas-fekete <89tamas.fekete@gmail.com>
2019-07-15 15:57:45 +0200

    added canvas and pixel coordinates

--- bmp.c
+++ bmp.c
@@ -5,8 +5,10 @@
 #include <unistd.h>
 #pragma pack ( 1 )
 
-#define height 600
-#define width 800
+#define height 768
+#define width 1024
+#define canvasheight 2.598
+#define canvaswidth 3.464
 #define bytesperpixel 3
 
 typedef struct{
@@ -50,19 +52,19 @@ typedef struct{
 }color_t;
 
 typedef struct{
- int sx;
- int sy;
- int sz;
- int r;
+ float sx;
+ float sy;
+ float sz;
+ float r;
  color_t color; 
 }sphere_t;
 typedef struct{
-  int x1;
-  int y1;
-  int z1;
-  int x2;
-  int y2;
-  int z2;
+  float x1;
+  float y1;
+  float z1;
+  float x2;
+  float y2;
+  float z2;
 }line_t;
 
 void putpixel(BMP_t* image,int x, int y, char red, char green, char blue);
@@ -75,14 +77,15 @@ int main(void)
   unsigned int i, j;
   sphere_t sphere;
   line_t line;
-  sphere.sx  = 100;
-  sphere.sy  = 100;
-  sphere.sz  = 100;
-  sphere.r = 1000;
-  line.x1 = width/2;
-  line.y1 = height/2;
+  sphere.sx  = 1.732;
+  sphere.sy  = 1.3;
+  sphere.sz  = 3;
+  sphere.r = 1;
+  line.x1 = 1.732;
+  line.y1 = 1.299;
   line.z1 = 0;
   line.z2 = 1;
+
   image.header.Signature = 0x4D42;
   image.header.FileSize  = sizeof(image);
   image.header.reserved  = 0;
@@ -105,25 +108,26 @@ int main(void)
     return -1;
   }
   printf("fd: %d\n", fd);
-
+  printf("sizeof integer: %ld", sizeof(int));
   for(i=0; i<sizeof(image.pixeldata); i++)
   {
     image.pixeldata.datab[i]=0;
   }
- 
-  for(i=0; i<600; i++)
+  
+  printf("here\n");
+  for(i=0; i<height; i++)
   {
-    for(j=0; j<800; j++)
+    for(j=0; j<width; j++)
     {
-      line.x2 = j;
-      line.y2 = i;
+      line.x2 = ((float)j+0.5)*canvaswidth/width;
+      line.y2 =  ((float)i+0.5)*canvasheight/height;
       if(LineIntersectsSphere(&line, &sphere))
       {
-        putpixel(&image, j, 599-i, 0xA4, 0x69, 0xFF);
+        putpixel(&image, j, height-i-1, 0xA4, 0x69, 0xFF);
       }
       else
       {
-        putpixel(&image, j, 599-i, 0xFF, 0xFF, 0xFF);
+        putpixel(&image, j, height-i-1, 0xFF, 0xFF, 0xFF);
       }
     }
   }
@@ -155,9 +159,19 @@ void putpixel(BMP_t* image,int x, int y, char red, char green, char blue)
 }
 char LineIntersectsSphere(line_t *line, sphere_t *sphere)
 {
-  int a = (line->x2-line->x1)*(line->x2-line->x1)+(line->y2-line->y1)*(line->y2-line->y1)+(line->z2-line->z1)*(line->z2-line->z1);
-  int b = 2*((line->x2-line->x1)*(line->x1-sphere->sx) + (line->y2-line->y1)*(line->y1-sphere->sy) + (line->z2-line->z1)*(line->z1-sphere->sz));
-  int c = sphere->sx*sphere->sx+sphere->sy*sphere->sy+sphere->sz*sphere->sz+line->x1*line->x1+line->y1*line->y1+line->z1*line->z1-2*(sphere->sx*line->x1+sphere->sy*line->y1+sphere->sz*line->z1) - sphere->r*sphere->r;
+  float x1 = line->x1;
+  float x2 = line->x2;
+  float x3 = sphere->sx;
+  float y1 = line->y1;
+  float y2 = line->y2;
+  float y3 = sphere->sy;
+  float z1 = line->z1;
+  float z2 = line->z2;
+  float z3 = sphere->sz;
+  float r = sphere->r;
+  float a = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1);
+  float b = 2*((x2-x1)*(x1-x3) + (y2-y1)*(y1-y3) + (z2-z1)*(z1-z3));
+  float c = x3*x3 + y3*y3 + z3*z3 + x1*x1 + y1*y1 + z1*z1 - 2*(x3*x1 + y3*y1 + z3*z1) - r*r;
   if( (b*b-4*a*c) < 0)
   {
     return 0;
