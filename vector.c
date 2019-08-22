0d2461b23d3210e9b23cee7c14fbad469586a062
tamas.fekete <89tamas.fekete@gmail.com>
2019-08-22 19:58:26 +0200

    removed unused function LineIntersectsSphere

--- vector.c
+++ vector.c
@@ -21,52 +21,6 @@ float PointToPointDistance(point_t p1, point_t p2)
  return sqrt((p1.x - p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) + (p1.z-p2.z)*(p1.z-p2.z));
 
 }
-char LineIntersectsSphere(line_t *line, sphere_t *sphere, point_t* point)
-{ 
-  point_t p1;
-  point_t p2;
-  float x1 = line->p1.x;
-  float x2 = line->p2.x;
-  float x3 = sphere->center.x;
-  float y1 = line->p1.y;
-  float y2 = line->p2.y;
-  float y3 = sphere->center.y;
-  float z1 = line->p1.z;
-  float z2 = line->p2.z;
-  float z3 = sphere->center.z;
-  float r = sphere->r;
-  float a = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1);
-  float b = 2*((x2-x1)*(x1-x3) + (y2-y1)*(y1-y3) + (z2-z1)*(z1-z3));
-  float c = x3*x3 + y3*y3 + z3*z3 + x1*x1 + y1*y1 + z1*z1 - 2*(x3*x1 + y3*y1 + z3*z1) - r*r;
-  if( (b*b-4*a*c) < 0)
-  {
-    return 0;
-  }
-  else // calculate the point of intersection(s) choose the point closer to the POV
-  {
-    float t1, t2;
-    float px1, py1, pz1, px2, py2, pz2;
-    t1 = (-b+sqrt( b*b - 4*a*c) ) / (2*a);
-    t2 = (-b-sqrt( b*b - 4*a*c) ) / (2*a);
-    p1.z  = z1 + t1*(z2 - z1);   
-    p2.z  = z1 + t2*(z2 - z1);
-    p1.y  = y1 + t1*(y2 - y1);
-    p2.y  = y1 + t2*(y2 - y1);
-    p1.x  = x1 + t1*(x2 - x1);
-    p2.x  = x1 + t2*(x2 - x1); //theres gotta be an easier way
-
-
-    if(PointToPointDistance(p1, line->p1) < PointToPointDistance(p2, line->p1))
-    {
-      *point = p1;
-    }
-    else
-    {
-      *point = p2;
-    }
-    return 1;
-  } 
- }
 sphere_t* Intersection(line_t *line)
 {
   point_t p1;
